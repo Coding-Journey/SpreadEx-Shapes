b984966f6f1fe75a0126b7e2dc757a669544e2cc
Will Wright <will.wright47@yahoo.co.uk>
2021-01-15 20:47:46 +0000

    Setting Up Project Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpreadEx_Shapes_WillW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Shapes!");
+        }
+    }
+}
